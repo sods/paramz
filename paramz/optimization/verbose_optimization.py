a5a0be15b12c5864b438d870b519ad17cc72cd12
Connor Fuhrman <connor@episci.com>
2023-03-17 07:55:55 -0700

    Removed numpy type aliases depreciated as of numpy 1.20.0

--- paramz/optimization/verbose_optimization.py
+++ paramz/optimization/verbose_optimization.py
@@ -35,7 +35,7 @@ import time
 
 
 def exponents(fnow, current_grad):
-    exps = [np.abs(np.float(fnow)),
+    exps = [np.abs(float(fnow)),
             1 if current_grad is np.nan else current_grad]
     return np.sign(exps) * np.log10(exps).astype(int)
 
